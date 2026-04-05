a6d9407bce37805170953b2a48992166f45355cf
mobin-alz <mobin.alizadeh2019@gmail.com>
2026-04-05 13:37:18 +0330

    add sql-migrate to handle database migrations
    feat(README.md): migrations setup

--- repository/mysql/migrations/1775345466_add_user_table.sql
+++ repository/mysql/migrations/1775345466_add_user_table.sql
@@ -1,6 +1,10 @@
+-- +migrate Up
 CREATE TABLE `users` (
                          id int PRIMARY KEY AUTO_INCREMENT,
                          name varchar(255),
                          phone_number varchar(255) not null UNIQUE,
                          create_at timestamp DEFAULT CURRENT_TIMESTAMP
 );
+
+-- +migrate Down
+DROP TABLE `users`;
--- repository/mysql/migrations/1775345487_add_password_column_to_user_table.sql
+++ repository/mysql/migrations/1775345487_add_password_column_to_user_table.sql
@@ -1,1 +1,5 @@
+-- +migrate Up
 ALTER TABLE users add column password varchar(255) not null;
+
+-- +migrate Down
+ALTEr TABLE users drop column password;
